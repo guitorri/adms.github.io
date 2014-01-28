e5efe4b89e35f97bd42ceec5b62e9ff80a0e831c
Guilherme Brondani Torri <guitorri@gmail.com>
2014-01-28 10:12:20 -0800

    add script to run Pandoc, convert .md to .html
     * depends on Pandoc http://johnmacfarlane.net/pandoc/

---
+++ convert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Simple script to convert Markdown into HTML
+
+# find Markdown files
+for fin in $( ls *.md); do
+  echo in: $fin
+
+  # strip path
+  fout=$(basename "$fin")
+
+  # replace extension
+  fout="${fout%.*}".html
+
+  echo out: $fout.html
+
+  cmd="pandoc $fin -o $fout"
+  echo $cmd
+
+  # run command
+  $cmd
+done
